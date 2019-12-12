9df07167f6f36af4a01349461a231aa79430d388
Haoyao Chen <taylorchenca@gmail.com>
2019-12-11 22:02:29 -0800

    Modified formatting and debug message.

--- get-grades.py
+++ get-grades.py
@@ -6,9 +6,8 @@ driver.get("https://ecampus.scu.edu/")
 
 userid = ''
 pwd = ''
+
 page_loading = True
-
-
 while page_loading:
     try:
         driver.find_element_by_id("userid").send_keys(userid)
@@ -25,7 +24,7 @@ while page_loading:
         driver.get('https://ecampus.scu.edu/psc/csprd92/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_MY_ACAD.GBL?Page=SSS_MY_ACAD&Action=U')
         page_loading = False
     except:
-        print("Page loading. Will try again in 5 sec")
+        print("Page loading. Will try again")
         time.sleep(3)
 
 page_loading = True
@@ -34,7 +33,7 @@ while page_loading:
         driver.find_element_by_id('win0divDERIVED_SSSACA2_SS_UNOFF_TRSC_LINK').click()
         page_loading = False
     except:
-        print("Page loading. Will try again in 5 sec")
+        print("Page loading. Will try again")
         time.sleep(1)
 
 page_loading = True
@@ -46,7 +45,7 @@ while page_loading:
         driver.find_element_by_id('GO').click()
         page_loading = False
     except:
-        print("Page loading. Will try again in 5 sec")
+        print("Page loading. Will try again")
         time.sleep(5)
 
 # driver.close()
